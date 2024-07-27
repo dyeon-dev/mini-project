dd396d269cb5a54f16d5a3a8cc67a85c7960962f
kimdayeon37 <kdy37912@naver.com>
2024-07-27 23:27:21 +0900

    프로젝트 배포 설정

--- src/main/java/com/ureka/myspring/SmartAccountApplication.java
+++ src/main/java/com/ureka/myspring/SmartAccountApplication.java
@@ -2,10 +2,19 @@ package com.ureka.myspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartAccountApplication {
+public class SmartAccountApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SmartAccountApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SmartAccountApplication.class, args);
     }
 }
+
